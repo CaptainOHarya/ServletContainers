c47fd8e99350af4bb4904a28477b5b5b07b8bfb1
Leonid Zulin <leo.zulin@gmail.com>
2023-04-04 21:06:08 +0300

    Using Java Config

--- servlets/src/main/java/ru/netology/service/PostService.java
+++ servlets/src/main/java/ru/netology/service/PostService.java
@@ -1,3 +1,4 @@
+
 package ru.netology.service;
 
 import ru.netology.exception.NotFoundException;
@@ -7,35 +8,34 @@ import ru.netology.repository.PostRepository;
 import java.util.List;
 
 public class PostService {
-  private final PostRepository repository;
+    private final PostRepository repository;
 
-  public PostService(PostRepository repository) {
-    this.repository = repository;
-  }
-
-  public List<Post> all() {
-    return repository.all();
-  }
-
-  public Post getById(long id) {
-    return repository.getById(id).orElseThrow(NotFoundException::new);
-  }
-
-
-  public Post save(Post post) {
-    if (post.getId() == 0) {
-      post.setId(repository.getNewId());
-      return repository.save(post);
-    } if (repository.isContainsId(post.getId())) {
-      return repository.change(post);
+    public PostService(PostRepository repository) {
+        this.repository = repository;
     }
-    throw new NotFoundException("Post not found!!!");
+
+    public List<Post> all() {
+        return repository.all();
+    }
+
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public synchronized Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(repository.getNewId());
+            return repository.save(post);
+        }
+        if (repository.isContainsId(post.getId())) {
+            return repository.change(post);
+        }
+        throw new NotFoundException("Post not found!!!");
 
 
-  }
+    }
 
-  public void removeById(long id) {
-    repository.removeById(id);
-  }
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
 }
-
